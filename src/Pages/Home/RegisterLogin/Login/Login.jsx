a5a566d6108d395ca1f93c57c8aba9b6a2697d13
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-05-28 04:34:37 +0600

    done login page

--- src/Pages/Home/RegisterLogin/Login/Login.jsx
+++ src/Pages/Home/RegisterLogin/Login/Login.jsx
@@ -1,34 +1,33 @@
-
+import login from '../../../../assets/Images/Home/login.png'
 
 const Login = () => {
     return (
         <div>
-            <div className="hero min-h-screen bg-base-200">
-                <div className="hero-content flex-col lg:flex-row-reverse">
-                    <div className="text-center lg:text-left">
-                        <h1 className="text-5xl font-bold">Login now!</h1>
-                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+            <div className="bg-[#E6E6E6] w-[90%] mx-auto my-12 rounded-lg p-7">
+                <div className="flex flex-col items-center lg:flex-row gap-10">
+                    <div className="text-center w-[50%] lg:text-left bg-[#07332F] ">
+                        <img className='w-full' src={login} alt="login" />
                     </div>
-                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                        <div className="card-body">
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Email</span>
-                                </label>
-                                <input type="text" placeholder="email" className="input input-bordered" />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Password</span>
-                                </label>
-                                <input type="text" placeholder="password" className="input input-bordered" />
-                                <label className="label">
-                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                                </label>
-                            </div>
-                            <div className="form-control mt-6">
-                                <button className="btn btn-primary">Login</button>
-                            </div>
+                    <div className="w-[50%] bg-white p-8 rounded-lg">
+                        <div className="form-control">
+                            <h1 className="text-4xl font-semibold text-center my-5">Sign in to Doc House</h1>
+                            <label className="label">
+                                <span className="label-text font-bold">Username or Email Address</span>
+                            </label>
+                            <input type="text" placeholder="Enter your username or email address" className="input input-bordered bg-slate-100" />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover text-red-500">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text font-bold">Password</span>
+                            </label>
+                            <input type="password" placeholder="Enter your password" className="input input-bordered bg-slate-100" />
+
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-error text-white">Login</button>
                         </div>
                     </div>
                 </div>
